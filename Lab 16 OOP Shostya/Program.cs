1b6339e17356b7a6f8aacb354f1fb7f7d79b47de
Denis8871 <145900110+Denis8871@users.noreply.github.com>
2024-04-23 15:24:44 +0300

    Program.cs

--- Lab 16 OOP Shostya/Program.cs
+++ Lab 16 OOP Shostya/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Lab_16_OOP_Dikiy
+namespace Lab_16_OOP_Shostya
 {
     internal static class Program
     {
